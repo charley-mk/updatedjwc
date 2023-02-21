93dff1bd43759dd646f5b8cdb2cc888c58110d23
charley-mk <charlesmk792@gmail.com>
2023-02-21 23:07:12 +0300

    added

---
+++ layout/footer.php
@@ -0,0 +1,8 @@
+    <!-- End your project here-->
+
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Custom scripts -->
+    <script type="text/javascript"></script>
+  </body>
+</html>
